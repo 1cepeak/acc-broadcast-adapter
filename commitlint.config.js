ebfc62bfb3eebe44c6757527787bd36f1347274a
1cepeak <foxwel.info@gmail.com>
2025-08-26 06:57:09 +0300

    ci(semantic-release): disable changelog autogeneration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,3 @@
 export default {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-max-length': [0, 'always'],
-    'body-max-length': [0, 'always'],
-    'body-max-line-length': [0, 'always'],
-    'footer-max-length': [0, 'always'],
-    'footer-max-line-length': [0, 'always'],
-    'subject-max-length': [0, 'always'],
-    'type-max-length': [0, 'always'],
-  },
 };
