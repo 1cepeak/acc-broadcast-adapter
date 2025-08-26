4bd1b52090879a772a80840dbcafcbdecc3744cb
1cepeak <foxwel.info@gmail.com>
2025-08-26 06:45:18 +0300

    chore(commitlint): commit length limits removed

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,12 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-line-length': [0, 'always'],
+    'body-max-length': [0, 'always'],
+    'body-max-line-length': [0, 'always'],
+    'footer-max-length': [0, 'always'],
+    'footer-max-line-length': [0, 'always'],
+    'subject-max-length': [0, 'always'],
+    'type-max-length': [0, 'always'],
+  },
 };
